70c95a83579a2a56a95f58579fbb7df4b3544f4f
Brayan Frank <brayanfrank.sm@gmail.com>
2022-08-21 22:59:21 -0300

    new branch

--- api/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "development": {
-    "username": "brau",
-    "password": "G^#65Acs&488",
-    "database": "english_school",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
